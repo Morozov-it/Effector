58d308598f6eda1a8bf269f8cb54fa33e9357a52
defpmportal.2.0 <morozov.it2020@gmail.com>
2022-04-14 12:13:20 +0300

    effector state

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,28 @@
-// Standard interface and functions
+import {createStore, createEvent} from 'effector'
+
 export interface Todo {
   id: number;
   text: string;
   done: boolean;
 }
 
-export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
+// Standard interface and functions
+const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     text: todo.id === id ? text : todo.text,
   }));
 
-export const toggleTodo = (todos: Todo[], id: number): Todo[] =>
+const toggleTodo = (todos: Todo[], id: number): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     done: todo.id === id ? !todo.done : todo.done,
   }));
 
-export const removeTodo = (todos: Todo[], id: number): Todo[] =>
+const removeTodo = (todos: Todo[], id: number): Todo[] =>
   todos.filter((todo) => todo.id !== id);
 
-export const addTodo = (todos: Todo[], text: string): Todo[] => [
+const addTodoToList = (todos: Todo[], text: string): Todo[] => [
   ...todos,
   {
     id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
@@ -28,3 +30,32 @@ export const addTodo = (todos: Todo[], text: string): Todo[] => [
     done: false,
   },
 ];
+
+//Effector state implementation
+
+//модель хранилища
+type Store = {
+  todos: Todo[],
+  newTodo: string
+}
+
+//события
+const setNewTodo = createEvent<string>()
+const addTodo = createEvent()
+
+//хранилище
+const store = createStore<Store>({
+  //исходное состояние
+  todos: [],
+  newTodo: ''
+})
+  //actions
+  .on(setNewTodo, (state, newTodo) => ({
+    ...state,
+    newTodo
+  }))
+  .on(addTodo, (state) => ({
+    ...state,
+    newTodo: '',
+    todos: addTodoToList(state.todos, state.newTodo)
+  }))
